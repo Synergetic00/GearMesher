2ff87fc73412f4e8b5c0bda688796095a34f2dde
Synergetic00 <evanjgillard@gmail.com>
2022-02-19 14:41:04 +1100

    temp files

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-from clipper import *
-from wrapper import *
-import os
+import math
+import svgutils
+from clipper import Point
 from xml.dom import minidom
 from svg.path import parse_path
-import svgutils
+from wrapper import Clipper, PolyFillType, PolyType, ClipType, SVGBuilder
 
-# def getRotatedSVG(pathSVG, degrees=0):
-#     svg = svgutils.transform.fromfile(pathSVG)
-#     originalSVG = svgutils.compose.SVG(pathSVG)
-#     originalSVG = originalSVG.rotate(degrees, x=324.5998, y=317.5917)
-#     # originalSVG.move(svg.height, 10)
-#     figure = svgutils.compose.Figure(svg.height, svg.width, originalSVG)
-#     figure.save('temp.svg')
+def rotateSvgFile(pathSVG, degrees, middle):
+    svg = svgutils.transform.fromfile(pathSVG)
+    originalSVG = svgutils.compose.SVG(pathSVG)
+    originalSVG = originalSVG.rotate(degrees, x=middle[0], y=middle[1])
+    figure = svgutils.compose.Figure(svg.height, svg.width, originalSVG)
+    figure.save('temp.svg')
 
-def getPointArrayTempo(tupleArray, xoff=0, yoff=0):
+def convertToPointArray(tupleArray, xoff=0, yoff=0):
     output = []
     for tup in tupleArray:
         pt = Point(x=tup[0] + xoff, y=tup[1] + yoff)
         output.append(pt)
     return output
 
-def loadSVGShit(pathSVG, rot=0):
+def getSampledSvgPoints(pathSVG, samples=2000):
     doc = minidom.parse(pathSVG)
     path_strings = [path.getAttribute('d') for path in doc.getElementsByTagName('path')]
     doc.unlink()
     svg_path = path_strings[-1]
     bezier_path = parse_path(svg_path)
-    num_of_samples = 2000
+    num_of_samples = samples
     pts = [ (p.real,p.imag) for p in (bezier_path.point(i/num_of_samples) for i in range(0, num_of_samples+1))]
     upper, left, right, lower = pts[0][1], pts[0][0], 0, 0
     for point in pts:
@@ -41,29 +40,17 @@ def loadSVGShit(pathSVG, rot=0):
     middle = (int((right-left)/2), int((lower-upper)/2))
     return (upper, lower, left, right, middle), pts
 
-
-# subjSVG = loadSVGShit('circle.svg', 40)
-# clipSVG = loadSVGShit('gear.svg', 40)
-# subjArr = getPointArrayTempo(subjSVG, 0, 0)
-# clipArr = getPointArrayTempo(clipSVG, 0, 0)
-
-
-################
-# Boiler Plate #
-################
-
-# clipSVG = loadSVGShit('gear.svg')
-
-def subFromSVG(diam, angle):
+def subFromSVG(radii, angle):
     subj, clip = [], []
-    cpoints, clipSVG = loadSVGShit('gear.svg')
-    _, subjSVG = loadSVGShit('circle.svg')
-    subjArr = getPointArrayTempo(subjSVG, 0, 0)
+    cpoints, clipSVG = getSampledSvgPoints('gear.svg')
+    rotateSvgFile('gear.svg', 0, cpoints[4])
+    _, subjSVG = getSampledSvgPoints('output.svg')
+    subjArr = convertToPointArray(subjSVG, 0, 0)
     dxoff = -int(cpoints[2]) - cpoints[4][1]
     dyoff = -int(cpoints[0]) - cpoints[4][0]
-    cxoff = dxoff + ((diam/2.) * math.cos(math.radians(angle)))
-    cyoff = dyoff + ((diam/2.) * math.sin(math.radians(angle)))
-    clipArr = getPointArrayTempo(clipSVG, cxoff, cyoff)
+    cxoff = dxoff + (radii * math.cos(math.radians(angle)))
+    cyoff = dyoff + (radii * math.sin(math.radians(angle)))
+    clipArr = convertToPointArray(clipSVG, cxoff, cyoff)
     subj.append(subjArr)
     clip.append(clipArr)
     c = Clipper()
@@ -81,7 +68,17 @@ def subFromSVG(diam, angle):
     svgBuilder.AddPolygons(solution, 0x60138013, 0xFF003300)
     svgBuilder.SaveToFile('./output.svg', invScale, 100)
 
-# for i in range(0, 628, 10):
-#     x_offset = ((800/2.) * math.cos(i/100))
-#     y_offset = ((800/2.) * math.sin(i/100))
-subFromSVG(1000, 45)
+def performSetup(pathSVG, radii):
+    svg = svgutils.transform.fromfile(pathSVG)
+    originalSVG = svgutils.compose.SVG(pathSVG)
+    size, _ = getSampledSvgPoints('frankenstein.svg')
+    scalew = radii / size[3]
+    scaleh = radii / size[1]
+    originalSVG = originalSVG.scale(x=scalew, y=scaleh)
+    figure = svgutils.compose.Figure(svg.height, svg.width, originalSVG)
+    figure.save('output.svg')
+
+RADIUS = 1000
+performSetup('frankenstein.svg', RADIUS)
+subFromSVG(RADIUS, 0)
+subFromSVG(RADIUS, 90)
